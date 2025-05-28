d66afe7b909bfd0c0e3e6df3c676bdf0e81d1286
cefireazure2505 <cefire.azure.2505@gmail.com>
2025-05-28 19:38:22 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 // Recuperar variables de entorno
-$dbHost = getenv('DB_HOST');         // e.g. mysql-cefire01.mysql.database.azure.com
-$dbName = getenv('DB_NAME');         // e.g. mi_basedatos
-$dbUser = getenv('DB_USER');         // e.g. adminuser@mysql-cefire01
-$dbPass = getenv('DB_PASS');         // tu contraseña
+$dbHost = getenv('DB_HOST');
+$dbName = "prueba";         
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
 
 echo $dbHost . " " . $dbName . " " . $dbUser . " " . $dbPass;
 
