2c27a135d3e295d713d0414cd26252187436208b
cefireazure2505 <cefire.azure.2505@gmail.com>
2025-05-28 19:36:13 +0200

    Update prueba_conexion.php

--- prueba_conexion.php
+++ prueba_conexion.php
@@ -5,13 +5,5 @@ $dbName = getenv('DB_NAME');         // e.g. mi_basedatos
 $dbUser = getenv('DB_USER');         // e.g. adminuser@mysql-cefire01
 $dbPass = getenv('DB_PASS');         // tu contraseña
 
-if (!$dbHost || !$dbName || !$dbUser || $dbPass === false) {
-    throw new \RuntimeException('Faltan variables de entorno para la conexión a la base de datos.');
-}
+echo $dbHost . " " . $dbName . " " . $dbUser . " " . $dbPass;
 
-//Establishes the connection
-$conn = mysqli_init();
-mysqli_real_connect($conn, $dbHost, $dbUser, $dbPass, $dbName, 3306);
-if (mysqli_connect_errno($conn)) {f
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
